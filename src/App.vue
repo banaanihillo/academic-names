76715caa1997b37db21989a606a0a60dc6eb98f0
vjhmur@utu.fi <vjhmur@utu.fi>
2021-01-08 15:24:33 +0200

    Added a list of names sorted by their amount

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1> Names </h1>
+        <Names />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Names from "./components/Names.vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: "App",
+        components: {
+            Names
+        },
+    };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: deeppink;
+    }
+
+    html {
+        background-color: black;
+    }
 </style>
---
+++ src/components/Names.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <ul class="names-container">
+            <li
+                v-for="item in names"
+                v-bind:key="item.name"
+            >
+                {{item.name}} ({{item.amount}})
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import mockDatabase from "../assets/names.json"
+
+    export default {
+        name: 'Names',
+        data() {
+            return {
+                names: mockDatabase.names.sort((item1, item2) => {
+                    return (item2.amount - item1.amount)
+                })
+            }
+        }
+    }
+</script>
+
+<!-- scoped -->
+<style scoped>
+    .names-container {
+        display: flex;
+        flex-flow: column nowrap;
+    }
+
+    .names-container li {
+        list-style: none;
+        outline: 1px solid darkmagenta;
+        padding: 1em;
+        margin: 1em;
+    }
+
+    ul {
+        padding: 0;
+    }
+</style>
